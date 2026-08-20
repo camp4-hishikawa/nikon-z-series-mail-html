3b3069f532d0db567cc4b5c583421de245d1c65c
菱川由理 <hishikawa@camp4.jp>
2026-08-20 09:21:52 +0900

    2608修正: CAMP4テストサーバをSSL対応したのでURL変更

--- src/_11ty/site.cjs
+++ src/_11ty/site.cjs
@@ -2,6 +2,6 @@ module.exports = {
   name: 'Nikon Zシリーズユーザーにおすすめ',
   copyright: 'Nikon Corporation / Nikon Imaging Japan Inc.',
   themeColor: '',
-  base: 'http://nij.nikon.camp4.tokyo',
+  base: 'https://nij.nikon.camp4.tokyo',
   path: '/cms/mail/cx/images/',
 }
